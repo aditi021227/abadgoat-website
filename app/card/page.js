f888c7f1d4c80f769b1b01e38e196bf2970bdbb2
aditi021227 <aditimalik279@gmail.com>
2025-08-23 12:58:18 +0530

    Fix CartContext imports in card and women pages

--- app/card/page.js
+++ app/card/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useCart } from "../../context/CartContext";
+import { useCart } from "@/app/context/CartContext";
 
 export default function CartPage() {
   const { cart, removeFromCart, updateQuantity } = useCart();
